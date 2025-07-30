2e608a690a87aa858b2dd7024b7f1718024ef4d1
Ankur Kumari Kushwaha <ankurkushwaha7408@gmail.com>
2025-07-30 23:36:48 +0530

    next function and errors along with res.send()

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get(/ab+c/, (req, res) => {
-    res.send({
-        name: "Ankur",
-        title: "Kushwaha"
-    })
-})
+app.get("/user", [(req, res, next) => {
+    next();
+}, (req, res, next) => {
+    console.log("User endpoint accessed");
+    next();
+}])
 
-app.post("/ab*c", (req, res) => {
-    res.send("sucessfully posted");
-})
-
-app.delete(/ab?c/, (req, res) => {
-    res.send("sucessfully deleted");
-})
-
-app.get("/user/:userID/:password", (req, res) => {
-    console.log("Query params are: ", req.params);
-    res.send("ends with fly");
-})
+app.get("/user", (req, res) => {
+    res.send("User endpoint response");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
